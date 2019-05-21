8890ca88db10c77376f96d9c1ece2880a8978f41
Alessandro Lepri <alex.lepri84@gmail.com>
2019-05-21 16:18:59 +0100

    Added URI

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 mongoose.plugin(require('mongoose-unique-validator'))
-const errorHandler = require('./lib/errorHandler')
-const routes = require('./config/routes')
+// const errorHandler = require('./lib/errorHandler')
+// const routes = require('./config/routes')
 
 
 const app = express()
@@ -13,10 +13,10 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
 
 app.use(bodyParser.json())
 
-app.use('/api', routes)
+// app.use('/api', routes)
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 
-app.use(errorHandler)
+// app.use(errorHandler)
 
 app.listen(process.env.PORT, () => console.log(`Up and running on port ${process.env.PORT}`))
 
